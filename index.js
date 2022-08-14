192e719b6d2c4f688afdfc7146da9fd4ef4334d0
Cynthia Oam <cynthiaoam@Cynthias-MacBook-Pro.local>
2022-08-14 10:51:17 -0700

    Added my project

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+//? make a double function that doubles the given argument and logs it
+// function doubleNumber(num) {
+//   console.log(num*2);
+// }
+// doubleNumber(100);
+
+
+
+//? make an increaseByHalf function at adds 50% of an argument and logs it
+
+// function increaseByHalf(myAmount) {
+//    console.log(myAmount*.5 + myAmount);
+//  }
+//  increaseByHalf(100);
+
+
+//? make an add function that adds 2 given argumnts and logs it
+// function isAdd(x,y) {
+//   console.log(x+y);
+// }
+// isAdd(3,4);
+
+
+
+//? make a wasHere function that returns a string arguement with "was here" concatenated onto it
+
+function wasHere(myName) {
+  return `${myName} was here`;
+}
+const answer = wasHere("Tom");
+const answer2 = wasHere("Jim")
+const answer3 = wasHere("Jerry")
+console.log(answer);
+console.log(answer2);
+console.log(answer3);
+
+//What are some data types?
+// String
+// Number
+// Boolean
+// null
+// undefined
+// float
+// double
+
+//What is an expression?
+// Statement example
+// let myNumber = 10 <- 10 in this case would be an expression
+
